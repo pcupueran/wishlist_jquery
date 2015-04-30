c431958dfda4a0af45222c3aeb19847f8e7111c8
pcupueran <pa1809ty@gmail.com>
2015-04-30 22:59:36 +0100

    Trigger button Add to List when pressing enter key

--- script.js
+++ script.js
@@ -10,6 +10,8 @@
 
 $(document).ready(function() {
 
+	$("#item").focus();
+
 	function addToList(value) {
 		var list = $("ol#items");   // it is $("ol#items") and not $("#items ol")
 		var wish = '<li class = "pending">' + value + 
@@ -26,10 +28,18 @@ $(document).ready(function() {
 	});
 	//what is the difference between using a "on" function and a normal function like the above and below ones?
 	$(document).on('click','.label.pending',function() {
+		console.log($(this));
 		$(this).switchClass("pending", "success");
 		$(this).text("Done!");
 		$(this).parent().switchClass("pending", "completed");
 		updateTotal();
+	});
+
+	$(document).keypress(function(e) {
+		if(e.which==13) {
+		 	button.trigger("click");
+			console.log("helkjdlfkjdklfjd");
+		}
 	});
 
 	function updateTotal() {
